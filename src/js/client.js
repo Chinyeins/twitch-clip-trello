0867aa055c04088ea99d0e09ad7ecaa7edeb593a
admin <chiny1@web.de>
2022-09-04 00:10:38 +0200

    test

--- src/js/client.js
+++ src/js/client.js
@@ -3,6 +3,7 @@
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
       let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+      
       return t
         .card("name")
         .get("name")
